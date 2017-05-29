0d40bc17533a001efe90164cad4b7cf8165c7d30
VicenteFleitas <vicente1fleitas1@gmail.com>
2017-05-28 21:46:04 -0400

    LocalStorage

--- js/Main.js
+++ js/Main.js
@@ -2,6 +2,7 @@ function iniciar()
 {
 	var boton = document.getElementById('grabar');
 	boton.addEventListener('click', nuevoitem, false);
+	window.addEventListener("storage", mostrar, false);
 
 	mostrar();
 }
@@ -11,40 +12,22 @@ function nuevoitem()
 	var clave = document.getElementById('clave').value;
 	var valor = document.getElementById('texto').value;
 
-	sessionStorage.setItem(clave, valor);
+	localStorage.setItem(clave, valor);
 	mostrar();
 	document.getElementById('clave').value = '';
 	document.getElementById('texto').value = '';
-
-	mostrar(clave);
 }
 
-function mostrar(clave)
+function mostrar()
 {
 	var cajadatos = document.getElementById('cajadatos');
-	cajadatos.innerHTML = '<div><buttom onclick="eliminarTodo()">Eliminar Todo</buttom></div>';
-	for (var i = 0; i < sessionStorage.length; i++) 
-	{
-		var clave = sessionStorage.key(i);
-		var valor = sessionStorage.getItem(clave);
-		cajadatos.innerHTML += '<div>' + clave + ' - ' + valor + '<br><buttom onclick="eliminar(\''+clave+'\')">Eliminar</buttom></div>';
-	}
-}
+	cajadatos.innerHTML = '';
 
-function eliminar(clave)
-{
-	if(confirm('Está seguro?'))
+	for(var i = 0; i < localStorage.length; i++)
 	{
-		sessionStorage.removeItem(clave);
-		mostrar();
-	}
-}
-function eliminarTodo()
-{
-	if(confirm('Está seguro?'))
-	{
-		sessionStorage.clear();
-		mostrar();
+		var clave = localStorage.key(i);
+		var valor = localStorage.getItem(clave);
+		cajadatos.innerHTML += '<div>' + clave + ' - ' + valor + '</div>';
 	}
 }
 
