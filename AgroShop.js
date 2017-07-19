bcadf4d6a3f707d7d6e4fc96e4185f08db47ff76
VicenteFleitas <vicente1fleitas1@gmail.com>
2017-07-18 21:33:17 -0400

    change screen 2

--- AgroShop.js
+++ AgroShop.js
@@ -918,7 +918,7 @@ $hxClasses["ApplicationMain"] = ApplicationMain;
 ApplicationMain.__name__ = ["ApplicationMain"];
 ApplicationMain.main = function() {
 	var projectName = "AgroShop";
-	var config = { build : "14", company : "Vicente Fleitas", file : "AgroShop", fps : 60, name : "AgroShop", orientation : "", packageName : "AgroShop", version : "1.0.0", windows : [{ allowHighDPI : false, alwaysOnTop : false, antialiasing : 0, background : 0, borderless : false, depthBuffer : false, display : 0, fullscreen : false, hardware : true, height : 0, hidden : null, maximized : null, minimized : null, parameters : { }, resizable : true, stencilBuffer : true, title : "AgroShop", vsync : false, width : 0, x : null, y : null}]};
+	var config = { build : "15", company : "Vicente Fleitas", file : "AgroShop", fps : 60, name : "AgroShop", orientation : "", packageName : "AgroShop", version : "1.0.0", windows : [{ allowHighDPI : false, alwaysOnTop : false, antialiasing : 0, background : 0, borderless : false, depthBuffer : false, display : 0, fullscreen : false, hardware : true, height : 800, hidden : null, maximized : null, minimized : null, parameters : { }, resizable : true, stencilBuffer : true, title : "AgroShop", vsync : false, width : 480, x : null, y : null}]};
 	lime_system_System.__registerEntryPoint(projectName,ApplicationMain.create,config);
 };
 ApplicationMain.create = function(config) {
@@ -27137,7 +27137,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 324292;
+	this.version = 385300;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = ["lime","utils","AssetCache"];
