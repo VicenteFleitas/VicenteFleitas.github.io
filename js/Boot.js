eb3e82edaa1ddd66918fdb5edf45be2c8346a61b
VicenteFleitas <vicente1fleitas1@gmail.com>
2017-07-24 22:34:18 -0400

    scale exact fit

--- js/Boot.js
+++ js/Boot.js
@@ -12,14 +12,27 @@ AgroShop.Boot.prototype =
 	create: function()
 	{
 		this.game.stage.backgroundColor = '#fff';
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		//this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.minWidth = 450;
 		this.scale.minHeight = 800;
 		this.scale.maxWidth = 2880;
 		this.scale.maxHeight = 1920;
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
-		this.game.scale.windowConstraints.bottom = "visual";
+		// this.scale.pageAlignHorizontally = true;
+		// this.scale.pageAlignVertically = true;
+		// this.game.scale.windowConstraints.bottom = "visual";
+		this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT; // Phaser.ScaleManager.SHOW_ALL USER_SCALE
+
+	    this.scale.pageAlignHorizontally = true;
+	    this.scale.pageAlignVertically = true;
+
+	    this.scale.setResizeCallback(function () 
+	    {
+	        var width = window.innerWidth;
+	        var height = window.innerHeight;
+	        console.log('size: ' + width + ', ' + height);
+	        this.camera.setSize(width, height);
+	        this.game.renderer.resize(width, height);
+	    }, this);
 
 		this.state.start('Preload');
 	}
