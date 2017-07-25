f538025873eda7966e7174e5027a7474d7e2ceca
VicenteFleitas <vicente1fleitas1@gmail.com>
2017-07-24 22:40:19 -0400

    x2

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,6 @@
 var AgroShop = AgroShop || {};
 
-AgroShop.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
+AgroShop.game = new Phaser.Game(window.innerWidth*2, window.innerHeight*2, Phaser.AUTO, '');
 AgroShop.game.state.add('Boot', AgroShop.Boot);
 AgroShop.game.state.add('Preload', AgroShop.Preload);
 AgroShop.game.state.add('Agroapp', AgroShop.Agroapp);
